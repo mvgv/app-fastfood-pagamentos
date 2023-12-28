f07b11abe56218ea75ac0cdb85c4b7c716745785
mvgv <mvgv1989@gmail.com>
2023-12-28 16:21:23 -0300

    feat: criacao da estrutura inicial do app

---
+++ app/src/test/java/br/com/appfastfoodpagamentos/AppFastfoodPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appfastfoodpagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFastfoodPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
